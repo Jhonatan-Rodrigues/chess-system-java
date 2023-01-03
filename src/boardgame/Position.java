32dff20311ec3185d3ee43ac96bf93ab85a1babc
Jhonatan Rodrigues <jhonatan07tr@gmail.com>
2023-01-03 16:09:19 -0300

    First class Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,35 @@
+package boardgame;
+
+public class Position {
+	
+	
+	private int rows;
+	private int column;
+	public Position(int rows, int column) {
+		super();
+		this.rows = rows;
+		this.column = column;
+	}
+	
+	public int getRows() {
+		return rows;
+	}
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+	public int getColumn() {
+		return column;
+	}
+	public void setColumn(int column) {
+		this.column = column;
+	}
+
+	@Override
+	public String toString() {
+		return rows + ", " + column; 
+ 				}
+	
+	
+	
+
+}
