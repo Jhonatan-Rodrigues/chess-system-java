af7a9f8a0accad9245b26cb94b35517e3446e4eb
Jhonatan Rodrigues <jhonatan07tr@gmail.com>
2023-01-03 16:29:00 -0300

    Starting to implement Board and Piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,21 @@
+package boardgame;
+
+public class Piece {
+
+	protected Position position;
+	private Board board;
+
+	public Piece(Board board) {
+		this.board = board;
+		this.position = null;
+	}
+
+	private Board getBoard() {
+		return board;
+	}
+
+	public void setBoard(Board board) {
+		this.board = board;
+	}
+
+}
